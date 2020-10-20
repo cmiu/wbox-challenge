c10c3ef241af42cf4fb860fa514d50430b31906c
unknown <miu.catalin21@gmail.com>
2020-10-21 00:29:14 +0300

    Replaced @RestController with @Controller

--- bancomat/src/main/java/com/wbox/bancomat/controller/BancomatController.java
+++ bancomat/src/main/java/com/wbox/bancomat/controller/BancomatController.java
@@ -3,13 +3,14 @@ package com.wbox.bancomat.controller;
 import com.wbox.bancomat.model.BanknoteType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.wbox.bancomat.service.BancomatService;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 public class BancomatController {
     @Autowired
     BancomatService bancomatService;
